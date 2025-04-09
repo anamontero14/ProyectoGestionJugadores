004e5717b8f84a6eb09040ffab257bfe15fa09b1
romansaborido <roman.saborido@iesnervion.es>
2025-04-09 12:23:55 +0200

    modelo

--- js/vista.js
+++ js/vista.js
@@ -1,26 +1,45 @@
 class Vista {
 
-    mostrarJugadores(jugadores) {
-        
-        for (let i in jugadores) {
+    constructor() {
+        // Contenedor donde se irán mostrando los jugadores
+        this.mostrarJugadores = document.getElementById("mostrarJugadoresAñadidos");
+        // Botón
+        this.boton = document.getElementById("boton");
+    }
 
-            let jugador = jugadores[i];
+    mostrarJugador(jugadores) {
+        console.log("Datos:", jugadores);
 
-            let jugadorFila = "<tr>" +
-            "<td>" + jugador.nombre + "</td>" +
-            "<td>" + jugador.posicion + "</td>" +
-            "<td>" + jugador.fechaNacimientos + "</td>" +
-            "</tr>";
+        // Limpiar el contenedor
+        this.mostrarJugadores.innerHTML = '';
 
-            $("#jugadores").append(jugadorFila);
+        // Crear lista ordenada
+        const ol = document.createElement('ol');
+
+        for (let i = 0; i < jugadores.length; i++) {
+            const jugadorItem = document.createElement('li');
+
+            // Mostrar información relevante del jugador
+            jugadorItem.textContent = `
+                Nombre: ${jugadores[i].nombre}, 
+                Edad: ${this.calcularEdad(jugadores[i].añoNacimiento)}, 
+                Posición: ${jugadores[i].posicion}
+            `;
+
+            ol.appendChild(jugadorItem);
         }
+
+        this.mostrarJugadores.appendChild(ol);
+    }
+
+    calcularEdad(añoNacimiento) {
+        const añoActual = new Date().getFullYear();
+        return añoActual - new Date(añoNacimiento).getFullYear();
     }
 
     clearInputs() {
         document.getElementById("nombreJugador").value = "";
-        document.getElementById("añoNacimientoJugador").value = "1000-01-01";
+        document.getElementById("añoNacimientoJugador").value = "";
         document.getElementById("posicionJugador").value = "";
     }
-
 }
-
