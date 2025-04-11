441a882ae95dcba67156f8b7728ce9c8a1206b5d
ana.montero <ana.montero@iesnervion.es>
2025-04-11 08:59:41 +0200

    Comentarios en la vista de la funcion de render jugadores y cambios en los id de los html

--- js/vista.js
+++ js/vista.js
@@ -6,22 +6,38 @@ class Vista {
         // Botón
         this.boton = document.getElementById("boton");
     }
-    
+
+    //funcion que hace un renderizado de los jugadores
     renderJugadores(jugadores) {
 
         console.log("Datos:", jugadores);
 
         // Limpiar el contenedor
-        $("#cuerpoTabla").empty();
+        $("#cuerpoTablaJugadores").empty();
 
+        //bucle for para recorrer el array de jugadores
         for (let i = 0; i < jugadores.length; i++) {
 
-            //creo las variables que almacenan los datos de las tablas
-            //const fila = document.createElement('tr');
-
+            /*variable que almacena el nº de la posición actual + 1 para
+            para que no aparezca como el primer nº el 0*/
             let tdId = i + 1;
+
+            /**
+             * una variable que almacena el nombre del jugador que se 
+             * encuentra en dicha posición
+             */
             let tdNombre = jugadores[i].nombre;
+
+            /**
+             * variable que almacena el año de nacimiento del jugador
+             * que se encuentra en esa posición
+             */
             let tdFechaNacimiento = jugadores[i].añoNacimiento;
+
+            /**
+             * variable que almacena la posición (al jugar) del
+             * jugador que se encuentre en esa posición del array
+             */
             let tdPosicion = jugadores[i].posicion;
 
             const fila = "<tr>" +
@@ -31,7 +47,37 @@ class Vista {
                 "<td>" + tdPosicion + "</td>" +
                 "<td>" + "<button type='button' id='" + i + "'>Eliminar</button>" + "</td>" + "</tr>";
 
-            $("#cuerpoTabla").append(fila);
+            $("#cuerpoTablaJugadores").append(fila);
+        }
+
+    }
+
+    //funcion de render para renderizar los equipos
+    renderEquipos(equipos) {
+
+        console.log("Datos:", equipos);
+
+        // Limpiar el contenedor
+        $("#cuerpoTablaEquipos").empty();
+
+        for (let i = 0; i < equipos.length; i++) {
+
+            //creo las variables que almacenan los datos de las tablas
+            //const fila = document.createElement('tr');
+
+            let tdId = i + 1;
+            let tdNombre = equipos[i].nombre;
+            let tdCiudad = equipos[i].ciudad;
+            let tdEstadio = equipos[i].estadio;
+
+            const fila = "<tr>" +
+                "<td>" + tdId + "</td>" +
+                "<td>" + tdNombre + "</td>" +
+                "<td>" + tdCiudad + "</td>" +
+                "<td>" + tdEstadio + "</td>" +
+                "<td>" + "<button type='button' id='" + i + "'>Eliminar</button>" + "</td>" + "</tr>";
+
+            $("#cuerpoTablaEquipos").append(fila);
         }
 
     }
@@ -44,8 +90,8 @@ class Vista {
 
     //Se encarga de limpiar los inputs cada vez que se pulsa el botón de enviar
     clearInputs() {
-        document.getElementById("nombreJugador").value = "";
-        document.getElementById("añoNacimientoJugador").value = "";
-        document.getElementById("posicionJugador").value = "";
+        document.getElementById("nombreEquipo").value = "";
+        document.getElementById("ciudadEquipo").value = "";
+        document.getElementById("estadioEquipo").value = "";
     }
 }
