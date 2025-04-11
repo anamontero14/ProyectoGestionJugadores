2c5eca17ff960b83c8fa88ae6a8d3c916ba76df5
ana.montero <ana.montero@iesnervion.es>
2025-04-11 08:37:05 +0200

    Índice arreglado

--- js/vista.js
+++ js/vista.js
@@ -19,7 +19,7 @@ class Vista {
             //creo las variables que almacenan los datos de las tablas
             //const fila = document.createElement('tr');
 
-            let tdId = i;
+            let tdId = i + 1;
             let tdNombre = jugadores[i].nombre;
             let tdFechaNacimiento = jugadores[i].añoNacimiento;
             let tdPosicion = jugadores[i].posicion;
